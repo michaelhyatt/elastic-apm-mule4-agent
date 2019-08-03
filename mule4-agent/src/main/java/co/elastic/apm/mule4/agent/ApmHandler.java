c9f895d9f6b22d2a4329303b5a8e6d9c4f429cc8
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-08-04 09:18:40 +1000

    Added skeleton methods for handling events

--- mule4-agent/src/main/java/co/elastic/apm/mule4/agent/ApmHandler.java
+++ mule4-agent/src/main/java/co/elastic/apm/mule4/agent/ApmHandler.java
@@ -16,11 +16,41 @@ public class ApmHandler {
 			InterceptionEvent event) {
 		logger.trace("Handling start event");
 		
+		if (isFirstEvent(location, parameters, event))
+			startTransaction(location, parameters, event);
+		else
+			startSpan(location, parameters, event);
+		
+	}
+
+	private void startSpan(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
+			InterceptionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void startTransaction(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
+			InterceptionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private boolean isFirstEvent(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
+			InterceptionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	public void handleEndEvent(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
 			InterceptionEvent event) {
 		logger.trace("Handling end event");
+		
+		endSpan(location, parameters, event);
+	}
+
+	private void endSpan(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
+			InterceptionEvent event) {
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -28,11 +58,25 @@ public class ApmHandler {
 			InterceptionEvent event) {
 		logger.trace("Handling exception event");
 		
+		captureException(location,parameters,event);
+	}
+
+	private void captureException(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
+			InterceptionEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public void handleSourceBeforeCallback(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
 			InterceptionEvent event) {
 		logger.trace("Handling source before callback event");
+		
+		endTransaction(location, parameters, event);
+	}
+
+	private void endTransaction(ComponentLocation location, Map<String, ProcessorParameterValue> parameters,
+			InterceptionEvent event) {
+		// TODO Auto-generated method stub
 		
 	}
 
