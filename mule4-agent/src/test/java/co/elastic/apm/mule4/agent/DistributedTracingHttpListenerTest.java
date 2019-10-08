92a7e2f454fd52cd49692aa92fa39bcb47719697
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-10-08 14:49:54 +1100

    Working test

--- mule4-agent/src/test/java/co/elastic/apm/mule4/agent/DistributedTracingHttpListenerTest.java
+++ mule4-agent/src/test/java/co/elastic/apm/mule4/agent/DistributedTracingHttpListenerTest.java
@@ -1,8 +1,10 @@
 package co.elastic.apm.mule4.agent;
 
-
 import static org.junit.Assert.assertEquals;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 
 import co.elastic.apm.mule4.agent.config.BaseAbstractApmMuleTestCase;
@@ -11,23 +13,26 @@ public class DistributedTracingHttpListenerTest extends BaseAbstractApmMuleTestC
 
 	@Test
 	public void testSimpleFlow() throws Exception {
-		flowRunner("Test").run();
-		
+
+		HttpGet getRequest = new HttpGet("http://127.0.0.1:8889/");
+		getRequest.addHeader("elastic-apm-traceparent", "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01");
+
+		HttpClient httpClient = HttpClientBuilder.create().build();
+
+		httpClient.execute(getRequest);
+
 		Thread.sleep(1000);
-		
+
 		assertEquals("Test", getTransaction().getNameAsString());
-//		assertEquals(6, getSpans().size());
-//		assertEquals("Logger1", getSpans().get(0).getNameAsString());
-//		assertEquals("Logger3", getSpans().get(1).getNameAsString());
-//		assertEquals("Logger4", getSpans().get(2).getNameAsString());
-//		assertEquals("Flow Reference2", getSpans().get(3).getNameAsString());
-//		assertEquals("Flow Reference1", getSpans().get(4).getNameAsString());
-//		assertEquals("Logger2", getSpans().get(5).getNameAsString());
+		assertEquals(2, getSpans().size());
+		assertEquals("Logger", getSpans().get(0).getNameAsString());
+		assertEquals("Set Payload", getSpans().get(1).getNameAsString());
+
 	}
 
 	@Override
 	protected String getConfigFile() {
 		return "DistributedTracingHTTPHeaderListenerTest.xml";
 	}
-	
+
 }
